8058ab187b95b6963dced990748a71de7e4914e8
Marat <lmar@mail.ru>
2019-09-09 18:16:09 +0300

    some improvements added

--- adressbook.py
+++ adressbook.py
@@ -15,38 +15,27 @@ import csv
 with open("phonebook_raw.csv", encoding="utf-8") as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-print(contacts_list)
+#print(contacts_list)
 
 # TODO 1: выполните пункты 1-3 ДЗ
 # ваш код
-res_contacts = []
-for elem in contacts_list:
-	lastn_patt = re.compile('(^([А-Яа-я])*)(\s)([А-Яа-я])')
-	if elem[0] != 'lastname':
-		k = ','.join(elem)
-		k = re.sub(r"(^([А-Яа-я])*)(\s)([А-Яа-я])", r"\1,\4", k)
-		#print('k: ', k)
-		elem = k.split(',')
-		#print('elem: ', elem)
-		res_contacts.append(elem)
-#print('=============================')
-#pprint(res_contacts)
 
-contacts_list = res_contacts.copy()
-res_contacts = []
+def my_replace(srch_pattrn, rpl_pattrn, mylist):
+	res_contacts = []
+	for elem in mylist:
+		if elem[0] != 'lastname':
+			k = ','.join(elem)
+			k = re.sub(srch_pattrn, rpl_pattrn, k)
+			elem = k.split(',')
+			res_contacts.append(elem)
+	return res_contacts
 
-for elem in contacts_list:
-	if elem[0] != 'lastname':
-		k = ','.join(elem)
-		k = re.sub(r",([А-Яа-я]*)( )([А-Яа-я]*)(,)", r",\1,\3", k)
-		#print('k: ', k)
-		elem = k.split(',')
-		#print('elem: ', elem)
-		res_contacts.append(elem)
-#print('=============================')
-#pprint(res_contacts)
-
-# pattern for phone '\+(\d)(\s*)(\(*)(\d*)(\)*)(\s*)(\d*)(\-*)(\d*)(\-*)(\d*)(\s*)(\(*)((доб.)*)(\s*)(\d*)'
+def my_uniq(mylist):
+	res_contacts = []
+	for elem in mylist:
+		for 
+		pass
+	return res_contacts
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
@@ -54,3 +43,24 @@ with open("phonebook.csv", "w", encoding="utf-8") as f:
   datawriter = csv.writer(f, delimiter=',')
   # Вместо contacts_list подставьте свой список
   datawriter.writerows(contacts_list)
+
+
+if __name__ == '__main__':
+	print(contacts_list)
+	# first call r"(^([А-Яа-я])*)(\s)([А-Яа-я])", r"\1,\4", k
+	rename = my_replace(
+		r"(^([А-Яа-я])*)(\s)([А-Яа-я])"
+		, r"\1,\4"
+		, contacts_list)
+	# second call r",([А-Яа-я]*)( )([А-Яа-я]*)(,)", r",\1,\3", k
+	rename1 = my_replace(
+		r",([А-Яа-я]*)( )([А-Яа-я]*)(,)"
+		, r",\1,\3"
+		, rename)
+	#print(rename1)
+	# last call r"(\+)*(\d)(\s)*(\(*)(\d\d\d)(\)*)(\s*)(\-*)(\d\d\d)(\-*)(\d\d)(\-*)(\d\d)(\s*)(\(*)(доб\.)*(\s*)(\d*)(\))*", r"+\2(\5)\9-\11-\13 \16\18", k
+	rename2 = my_replace(
+		r"(\+)*(\d)(\s)*(\(*)(\d\d\d)(\)*)(\s*)(\-*)(\d\d\d)(\-*)(\d\d)(\-*)(\d\d)(\s*)(\(*)(доб\.)*(\s*)(\d*)(\))*"
+		, r"+\2(\5)\9-\11-\13 \16\18"
+		, rename1)
+	print(rename2)
